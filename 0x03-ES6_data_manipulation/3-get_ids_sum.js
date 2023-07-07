1e619424ff19536a148d00ddb9f6ee4621bfb44b
Joseph Endale <josi.endal45@gmail.com>
2023-07-08 01:20:20 +0300

    Added Comment line and fixed export default function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,6 @@
-function getStudentIdsSum(students) {
-    return students.reduce((sum, student) => sum + student.id, 0);
+// The reduce method works with two functions called accumulator and current
+// The current value should be 0 by default
+// The accumulator adds upp all the specified valued
+export default function getStudentIdsSum(arr) {
+    return arr.reduce((acc, cur) => acc + cur.id, 0);
   }
